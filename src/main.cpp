24ae7c41742c3a5e89336781109f3d3f567f2225
Edu Pagotto <edupagotto@gmail.com>
2024-05-30 15:29:57 -0300

    melhorias

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "iostream"
+#include "config.hpp"
 
 int main(int argn, char** argv) {
 
     std::cout << "Hello stupid world" << std::endl;
+    std::cout << project_name << '\n';
+    std::cout << project_version << '\n';
 
     return 0;
 }
